ee7d0ea3d23ff878d1255b9d3c0b9b96bbc7ed2b
kazuyukirumaruma <67451150+kazuyukirumaruma@users.noreply.github.com>
2020-07-24 19:29:12 +0900

    user valid 2

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,17 +4,22 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+
   VALID_PASSWORD = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{6,100}+\z/i
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  VALID_NICKNAME = /\A[a-zA-Z]+\z/
   VALID_NAME = /\A[ぁ-んァ-ン一-龥]/
   VALID_KANA_NAME = /\A[ァ-ヶー－]+\z/
 
-   validates :password, presence: true, length: {minimum: 6}, format: { with: VALID_PASSWORD }, confirmation: true
-   validates :email, {presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }}
-   validates :nickname, presence: true
-   validates :firstname, presence: true, format: { with: VALID_NAME }
-   validates :lastname, presence: true, format: { with: VALID_NAME }
-   validates :kana_first, presence: true, format: { with: VALID_KANA_NAME }
-   validates :kana_last, presence: true, format: { with: VALID_KANA_NAME }
-   validates :birthday, presence: true
+  with_options presence: true do |s|
+
+    s.validates :password, length: {minimum: 6}, format: { with: VALID_PASSWORD }, confirmation: true
+    s.validates :email, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }}
+    s.validates :nickname, format: { with: VALID_NICKNAME }, uniqueness: { case_sensitive: false }}
+    s.validates :firstname, format: { with: VALID_NAME }
+    s.validates :lastname, format: { with: VALID_NAME }
+    s.validates :kana_first, format: { with: VALID_KANA_NAME }
+    s.validates :kana_last, format: { with: VALID_KANA_NAME }
+    s.validates :birthday
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,52 +3,56 @@ require 'rails_helper'
 describe User do
   describe '#create' do
 
+    before :context do
+      @user = FactoryBot.build(:user)
+    end
+
     it "is invalid without a nickname" do
-      user = build(:user, nickname: "")
-      user.valid?
-      expect(user.errors[:nickname]).to include("can't be blank")
+      @user.nickname =  ""
+      @user.valid?
+      expect(@user.errors[:nickname]).to include("can't be blank")
     end
 
     it "is invalid without an email" do
-      user = build(:user, email: "")
-      user.valid?
-      expect(user.errors[:email]).to include("can't be blank")
+      @user.email = ""
+      @user.valid?
+      expect(@user.errors[:email]).to include("can't be blank")
     end
 
     it "is invalid without a password" do
-      user = build(:user, password: "")
-      user.valid?
-      expect(user.errors[:password]).to include("can't be blank")
+      @user.password = ""
+      @user.valid?
+      expect(@user.errors[:password]).to include("can't be blank")
     end
 
     it "is invalid without a firstname" do
-      user = build(:user, firstname: "")
-      user.valid?
-      expect(user.errors[:firstname]).to include("can't be blank")
+      @user.firstname = ""
+      @user.valid?
+      expect(@user.errors[:firstname]).to include("can't be blank")
     end
 
     it "is invalid without a lastname" do
-      user = build(:user, lastname: "")
-      user.valid?
-      expect(user.errors[:lastname]).to include("can't be blank")
+      @user.lastname = ""
+      @user.valid?
+      expect(@user.errors[:lastname]).to include("can't be blank")
     end
 
     it "is invalid without a kana_first" do
-      user = build(:user, kana_first: "")
-      user.valid?
-      expect(user.errors[:kana_first]).to include("can't be blank")
+      @user.kana_first = ""
+      @user.valid?
+      expect(@user.errors[:kana_first]).to include("can't be blank")
     end
 
     it "is invalid without a kana_last" do
-      user = build(:user, kana_last: "")
-      user.valid?
-      expect(user.errors[:kana_last]).to include("can't be blank")
+      @user.kana_last = ""
+      @user.valid?
+      expect(@user.errors[:kana_last]).to include("can't be blank")
     end
 
     it "is invalid without a birthday" do
-      user = build(:user, birthday: "")
-      user.valid?
-      expect(user.errors[:birthday]).to include("can't be blank")
+      @user.birthday = ""
+      @user.valid?
+      expect(@user.errors[:birthday]).to include("can't be blank")
     end
 
     it "is invalid with a duplicate email address" do
@@ -68,5 +72,38 @@ describe User do
       user.valid?
       expect(user.errors[:password]).to include("is too short (minimum is 6 characters)")
     end
+
+    it "パスワードを二回入力しない場合 " do
+      user = build(:user, password: "000000", password_confirmation: "")
+      user.valid?
+      expect(user.errors[:password]).to include("is too short (minimum is 6 characters)")
+    end
+
+    it "メールアドレスは@を含む必要があること" do
+      @user.email = "kkkkkkkkkk"
+      @user.valid?
+      expect(@user.errors[:email]).to include("email require @")
+    end
+
+    it "パスワードは半角英数字混合であること" do
+      @user.password = "kkkkkkkkkk"
+      @user.valid?
+      expect(@user.errors[:password]).to include("password ")
+    end
+
+    it "ユーザー本名は全角（漢字・ひらがな・カタカナ）で入力させること" do
+      @user.firstname && lastname = "kkkkkkkkkkkk"
+      @user.valid?
+      expect(@user.errors[:firstname && lastname]).to include("password ")
+    end
+
+    it "ユーザー本名のフリガナは全角（カタカナ）で入力させること" do
+      @user.kana_first && kana_last = "kkkkkkkkkkkk"
+      @user.valid?
+      expect(@user.errors[:kana_first && kana_last]).to include("password ")
+    end
+
+
+    
   end
 end
